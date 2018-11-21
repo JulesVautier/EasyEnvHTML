e33566e04b38ca98ff528ee9c833321f7d101e93
jules vautier <jules.vautier@epitech.eu>
2018-11-21 11:40:07 +0100

    nodemon

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/development'));
 
 app.listen(port, function(){
     console.log('Express server listening on port ' + port);
 });
+
+
+
