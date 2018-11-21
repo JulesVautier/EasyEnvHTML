939825e4a6bf199692f82dbf8763fd64b15bf4c0
jules vautier <jules.vautier@epitech.eu>
2018-11-21 11:29:47 +0100

    add express server

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function(){
+    console.log('Express server listening on port ' + port);
+});
